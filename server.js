96f92fb1d5874fe2d089c74f30eb0d7cd654b188
Vlad Valchkov <62916716+VladOXPR@users.noreply.github.com>
2025-09-30 00:35:24 -0500

    Allow iframe embedding by changing X-Frame-Options from DENY to SAMEORIGIN

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(compression());
 // Security headers
 app.use((req, res, next) => {
     res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
     res.setHeader('X-XSS-Protection', '1; mode=block');
     next();
 });
